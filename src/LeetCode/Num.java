da1ca3c6074975fab2fbd7c4f9351b98dacf3c88
andeltas <2755839590@qq.com>
2020-11-08 14:18:55 +0800

    添加一句话

--- src/LeetCode/Num.java
+++ src/LeetCode/Num.java
@@ -24,6 +24,7 @@ public class Num {
         return tmp.size() == tmp.values().stream().distinct().count();
     }
     public static void main(String[] args) {
+        // 测试函数
         System.out.println(uniqueOccurrences(new int[]{1, 2, 2, 1, 1, 3}));
     }
 }
