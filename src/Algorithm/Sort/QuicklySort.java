c9c5af5e217279829fe74b203305d190624df7e0
andeltas <2755839590@qq.com>
2020-11-08 13:56:35 +0800

    添加LeetCode目录

--- src/Algorithm/Sort/QuicklySort.java
+++ src/Algorithm/Sort/QuicklySort.java
@@ -6,15 +6,27 @@ import java.util.ArrayList;
 import Tool.Fromfile.getInt;
 
 public class QuicklySort {
+    private static int scanOnce(ArrayList<Integer> data, int low, int high){
+        int index = data.get(low);
+        while (low < high){
+            while (low < high && data.get(high) >= index) high++;
+            data.set(low, data.get(high));
+            while (low < high && data.get(low) <= index) low++;
+            data.set(high, data.get(low));
+        }
+        data.set(high, index);
+        return high;
+    }
 
-    public static void sort(ArrayList<Integer> data) {
-        int i = data.get(0);
-        // TODO
+    public static void quicklySort(ArrayList<Integer> data, int low, int high) {
+        int index = scanOnce(data, low, high);
+        quicklySort(data, low, index - 1);
+        quicklySort(data, index + 1, high);
     }
 
     public static void main(String[] args) throws IOException {
         ArrayList<Integer> data = getInt.readInt("Note");
-        sort(data);
+        quicklySort(data, 0, data.size() - 1);
         for (Integer datum : data) {
             System.out.println(datum);
         }
